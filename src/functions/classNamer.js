4be1ee4b30b9cf500ee177c4e1abfc8311fc4c26
LuisGilGB <luis.gilgb@gmail.com>
2019-11-23 12:44:06 +0100

    Added protection against undefined inputs

--- src/functions/classNamer.js
+++ src/functions/classNamer.js
@@ -1,6 +1,6 @@
 /*
  * A function that joins different strings to form a single one that will be the final className.
  */
-const classNamer = (...classNames) => classNames.reduce((temp, s) => `${temp} ${s}`.trim(), '');
+const classNamer = (...classNames) => classNames.reduce((temp, s = '') => `${temp} ${s}`.trim(), '');
 
 export default classNamer;
