5431fdb43922d87939221739d89379e872021762
Luis Gil Gutiérrez de la Barreda <luis.gilgb@gmail.com>
2020-04-08 18:00:46 +0200

    ClassNamer function rework to accept conditional adding with object params

--- src/functions/classNamer.js
+++ src/functions/classNamer.js
@@ -1,6 +1,12 @@
 /*
  * A function that joins different strings to form a single one that will be the final className.
  */
-const classNamer = (...classNames) => classNames.reduce((temp, s) => `${temp} ${s || ''}`.trim(), '');
+import {isObject, isNonEmptyString} from '@luisgilgb/js-utils';
+
+const concatIf = (s1 = '', s2 = '', cond) => cond ? `${s1} ${s2}`.trim() : s1;
+
+const iterate = (temp, s) => isObject(s) ? concatIf(temp, s.className, s.check) : concatIf(temp, s, isNonEmptyString(s));
+
+const classNamer = (...classNames) => classNames.reduce(iterate, '');
 
 export default classNamer;
